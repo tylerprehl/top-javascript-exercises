f3e319644973dcd226931362c5167b29696f3301
Tyler Prehl <tyler@prehl.us>
2023-07-18 15:52:10 -0400

    Completed exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let tempFahrenheit = 0.00;
+  tempFahrenheit = Math.round((temperature - 32) * (5/9) * 10) / 10;
+  return tempFahrenheit;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let tempCelsius = 0.00;
+  tempCelsius = Math.round(((temperature * (9/5) + 32) * 10)) / 10;
+  return tempCelsius;
 };
 
 // Do not edit below this line

38ebe38f92d297ba31ebc1cfbc35d40c36776935
Tyler Prehl <tyler@prehl.us>
2023-07-18 15:35:34 -0400

    Complete exercise 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let isLeapYear = false;
+    if (year % 400 === 0) { isLeapYear = true; }
+    else if (year % 4 === 0 && year % 100 !== 0) { isLeapYear = true; }
+    return isLeapYear;
 };
 
 // Do not edit below this line

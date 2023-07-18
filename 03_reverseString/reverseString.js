8a807f823e413fda86b2aa6fb45619509a932c39
Tyler Prehl <tyler@prehl.us>
2023-07-18 09:42:41 -0400

    Complete exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let finalString = "";
+    originalLength = text.length;
+    for (let i=0; i<originalLength; i++) {
+        finalString = finalString + text.substring(text.length-1, text.length);
+        text = text.substring(0, text.length-1);
+    }
+    return finalString;
 };
 
 // Do not edit below this line

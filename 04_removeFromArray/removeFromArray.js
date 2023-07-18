3bfe87d5fb4bc76e4141adf041a3f7d9e169872e
Tyler Prehl <tyler@prehl.us>
2023-07-18 10:11:10 -0400

    Complete exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray, ...values) {
+    for (let i=0; i<values.length; i++) {
+        console.log(`Value to Remove: ${values[i]}`);
+        console.log(`Type of Value: ${typeof values[i]}`);
+        if (inputArray.includes(values[i])) {
+            let valueIndex = inputArray.indexOf(values[i]);
+            console.log(`Index of Value in OG: ${valueIndex}`);
+            inputArray.splice(valueIndex, 1);
+        }
+    }
+    return inputArray;
 };
 
 // Do not edit below this line

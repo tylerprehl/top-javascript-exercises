328ae7c87939afe68f161ce24dd6ad38fd81b020
Tyler Prehl <tyler@prehl.us>
2023-07-18 09:36:49 -0400

    Complete exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(text, repeatNum) {
+    if(repeatNum < 0) {
+        return "ERROR";
+    }
+    let finalText = "";
+    for (let i = 0; i < repeatNum; i++) {
+        finalText = finalText + text;
+    }
+    return finalText;
 };
 
 // Do not edit below this line

db8cd1db7977df790f03777de2682db0ce6240f9
Tyler Prehl <tyler@prehl.us>
2023-08-28 22:17:51 -0400

    Complete exercise 10_fibonacci
    Solve the fibonacci exercise using newfound knowledge of arrays and
    their methods in JS.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(seqNum) {
+    if (seqNum < 0) {return "OOPS"}
 
+    seqNum = Number(seqNum);
+    
+    fibSequence = [1, 1];
+    for (let i=2; i<seqNum; i++) {
+        let num1 = fibSequence.slice(-1);
+        let num2 = fibSequence.slice(-2, -1);
+        fibSequence.push(num1[0]+num2[0]);        
+    }
+    console.log(`Fib Sequence: ${fibSequence}`);
+    return fibSequence.slice(-1)[0];
 };
 
 // Do not edit below this line

4c9a0e997c353f81f72b3e69403173cdf760f766
Tyler Prehl <tyler@prehl.us>
2023-08-29 08:55:20 -0400

    Complete exercise 12_findTheOldest
    Solve the exercise using knowledge of extracting data from objects and
    using the reduce Array method.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return oldestPerson = people.reduce((oldest, curPerson) => {
+        let oldestAge = 0;
+        let curPersonAge = 0;
+        
+        if (oldest["yearOfDeath"] === undefined) {
+            oldestAge = 2023 - oldest["yearOfBirth"];
+        } else {
+            oldestAge = oldest["yearOfDeath"] - oldest["yearOfBirth"];
+        }
 
+        if (curPerson["yearOfDeath"] === undefined) {
+            curPersonAge = 2023 - curPerson["yearOfBirth"];
+        } else {
+            curPersonAge = curPerson["yearOfDeath"] - curPerson["yearOfBirth"];
+        }
+
+        console.log(`Oldest: ${oldest["name"]}   Cur Person: ${curPerson["name"]}`);
+        console.log(`Oldest: ${oldestAge}   Cur Person: ${curPersonAge}`);
+
+        if (curPersonAge > oldestAge) { return curPerson; }
+        else { return oldest; }
+    })
 };
 
 // Do not edit below this line

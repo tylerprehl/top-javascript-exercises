f677ebb98e670a86ec788e7712572312d9d9721c
Tyler Prehl <tyler@prehl.us>
2023-08-28 21:56:19 -0400

    Complete exercise 09_palindromes
    Solve the palindromes exercise using knowledge of arrays and their
    methods.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (words) {
+    console.log(`Original: ${words}`);
+    words = words.replace(/[^A-Za-z0-9]/g, "");
+    words = words.toLowerCase();
+    let arrWords = words.split("");
+    arrWords.reverse();
+    let reversedWords = arrWords.join("");
+    console.log(`Words: ${words}`);
+    console.log(`Reversed: ${reversedWords}`);
+    if (words === reversedWords) { 
+        console.log("True");
+        return true; 
+    }
+    else { 
+        console.log("False");
+        return false; 
+    }
 };
 
 // Do not edit below this line
